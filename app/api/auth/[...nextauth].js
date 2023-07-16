02175d4fa14c6fbb3dac09c9beeb410a19a19d1c
Andral Orelus <andral.andrizzy@gmail.com>
2023-07-16 10:24:47 -0700

    get listings from api

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -1,4 +1,3 @@
-import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from '@/prisma/client'
@@ -7,7 +6,7 @@ export const NextAuth = ({
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENT_ID,
+            clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
